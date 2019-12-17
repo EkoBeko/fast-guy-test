d22e2b742cc356de065e3fdf5cc30f7184d2784f
EkoBeko <58846303+EkoBeko@users.noreply.github.com>
2019-12-17 21:23:58 +0300

    yazılar

---
+++ RedCube - Kopya/Assets/Scenes/story_enterance/dialog.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class dialog : MonoBehaviour {
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingspeed;
+    public GameObject conButton;
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingspeed);
+        }
+    }
+
+    public void nextSentence()
+    {
+        conButton.SetActive(false);
+        if(index<sentences.Length-1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            conButton.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            conButton.SetActive(true);
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+}
