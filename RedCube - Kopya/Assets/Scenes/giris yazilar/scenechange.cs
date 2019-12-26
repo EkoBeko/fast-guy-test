b241588d0927778d8c1a2b683b97977f3dbd1e4c
EkoBeko <58846303+EkoBeko@users.noreply.github.com>
2019-12-27 00:31:22 +0300

    giris sahnesi

---
+++ RedCube - Kopya/Assets/Scenes/giris yazilar/scenechange.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechange : MonoBehaviour
+{
+
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElaped;
+    private void Update()
+    {
+        timeElaped += Time.deltaTime;
+        if(timeElaped > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+
+
+
+
+
+
+}
