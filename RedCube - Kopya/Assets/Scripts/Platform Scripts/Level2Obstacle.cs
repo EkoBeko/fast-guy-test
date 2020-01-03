44d97eb991b7278f4224d90ecf5d1f5847955f78
EkoBeko <58846303+EkoBeko@users.noreply.github.com>
2020-01-03 22:06:10 +0300

    Level2NewUpdate

--- RedCube - Kopya/Assets/Scripts/Platform Scripts/Level2Obstacle.cs
+++ RedCube - Kopya/Assets/Scripts/Platform Scripts/Level2Obstacle.cs
@@ -8,7 +8,7 @@ public class Level2Obstacle : MonoBehaviour
     public float speed;
     private void Update()
     {
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
